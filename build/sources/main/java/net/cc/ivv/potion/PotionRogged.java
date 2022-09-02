40a6ccf2d08ff52c3b1adc294b3f4094d123624c
LauraDarkez <53153740+LauraDarkez@users.noreply.github.com>
2022-09-01 22:25:33 -0400

    github made me push after using gradlew build

---
+++ build/sources/main/java/net/cc/ivv/potion/PotionRogged.java
@@ -0,0 +1,108 @@
+
+package net.cc.ivv.potion;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import net.minecraft.world.World;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.potion.Potion;
+import net.minecraft.entity.ai.attributes.AbstractAttributeMap;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.Minecraft;
+
+import net.cc.ivv.procedure.ProcedureRoggedDuringEffect;
+import net.cc.ivv.procedure.ProcedureRoggedAfterEffect;
+import net.cc.ivv.ElementsIvvMod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@ElementsIvvMod.ModElement.Tag
+public class PotionRogged extends ElementsIvvMod.ModElement {
+	@GameRegistry.ObjectHolder("ivv:rogged")
+	public static final Potion potion = null;
+	public PotionRogged(ElementsIvvMod instance) {
+		super(instance, 10);
+	}
+
+	@Override
+	public void initElements() {
+		elements.potions.add(() -> new PotionCustom());
+	}
+	public static class PotionCustom extends Potion {
+		private final ResourceLocation potionIcon;
+		public PotionCustom() {
+			super(false, -2076);
+			setRegistryName("rogged");
+			setPotionName("effect.rogged");
+			potionIcon = new ResourceLocation("ivv:textures/items/items/flamingrod.png");
+		}
+
+		@Override
+		public boolean isInstant() {
+			return false;
+		}
+
+		@Override
+		public boolean shouldRenderInvText(PotionEffect effect) {
+			return true;
+		}
+
+		@Override
+		public boolean shouldRenderHUD(PotionEffect effect) {
+			return true;
+		}
+
+		@Override
+		public void performEffect(EntityLivingBase entity, int amplifier) {
+			World world = entity.world;
+			int x = (int) entity.posX;
+			int y = (int) entity.posY;
+			int z = (int) entity.posZ;
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("entity", entity);
+				ProcedureRoggedDuringEffect.executeProcedure($_dependencies);
+			}
+		}
+
+		@Override
+		public void removeAttributesModifiersFromEntity(EntityLivingBase entity, AbstractAttributeMap attributeMapIn, int amplifier) {
+			super.removeAttributesModifiersFromEntity(entity, attributeMapIn, amplifier);
+			World world = entity.world;
+			int x = (int) entity.posX;
+			int y = (int) entity.posY;
+			int z = (int) entity.posZ;
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("entity", entity);
+				ProcedureRoggedAfterEffect.executeProcedure($_dependencies);
+			}
+		}
+
+		@SideOnly(Side.CLIENT)
+		@Override
+		public void renderInventoryEffect(int x, int y, PotionEffect effect, Minecraft mc) {
+			if (mc.currentScreen != null) {
+				mc.getTextureManager().bindTexture(potionIcon);
+				Gui.drawModalRectWithCustomSizedTexture(x + 6, y + 7, 0, 0, 18, 18, 18, 18);
+			}
+		}
+
+		@SideOnly(Side.CLIENT)
+		@Override
+		public void renderHUDEffect(int x, int y, PotionEffect effect, Minecraft mc, float alpha) {
+			mc.getTextureManager().bindTexture(potionIcon);
+			Gui.drawModalRectWithCustomSizedTexture(x + 3, y + 3, 0, 0, 18, 18, 18, 18);
+		}
+
+		@Override
+		public boolean isReady(int duration, int amplifier) {
+			return true;
+		}
+	}
+}
